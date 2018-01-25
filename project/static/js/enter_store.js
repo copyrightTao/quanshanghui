da1b2f5fd57a56f06cf5ac1846d0ebadd09a4122
tgf <addevent@126.com>
2018-01-25 20:29:12 +0800

    修改开店铺JS验证条件 我的信息页面完成

--- project/static/js/enter_store.js
+++ project/static/js/enter_store.js
@@ -9,4 +9,32 @@ $(function(){
             elem: '#test2' //指定元素
           });
       });
+      $('.submit').click(function(){
+        $('.xieyi .content input').each(function(){
+            if(!$(this).val()){
+                var msg = $(this).siblings('span').html().substr(0,$(this).siblings('span').html().length-1)
+                alert(msg + '不能为空')
+                return false;
+            }
+        })
+        var PHONE_START = /0\d{2,3}/;//验证区号
+        var PHONE_END = /\d{7,8}/;//验证座机号码
+        var LICENSE_ID = /^[A-Z0-9]{15}$|^[A-Z0-9]{17}$|^[A-Z0-9]{18}$|^[A-Z0-9]{20}$/;//验证税号
+        var LAW_ID=/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;//身份证号
+        var BANK_ACCNO = /^([1-9]{1})(\d{15}|\d{18})$/;//验证银行卡号
+        alertMsg(PHONE_START,$('#SI_PHONE_START'));
+        alertMsg(PHONE_END,$('#SI_PHONE_END'));
+        alertMsg(LICENSE_ID,$('#SI_LICENSE_ID'));
+        alertMsg(LAW_ID,$('#SI_LAW_ID'));
+        alertMsg(BANK_ACCNO,$('#SI_BANK_ACCNO'));
+      })
+      function alertMsg(a,ele){
+         if(!ele.val()==""){
+            if(!a.test(ele.val())){
+                var msg = $(ele).siblings('span').html().substr(0,$(ele).siblings('span').html().length-1)
+                alert(msg + '输入有误')
+                return false;
+            }
+         }
+      }
 })
---
+++ project/static/js/myInformation.js
@@ -0,0 +1,37 @@
+$(function () {
+    //个人中心通用的样式修改start
+    $('.hengxian').remove();
+    $('.daohang').css('background-color', '#da0011');
+    $('.othercategory li').eq(2).remove();
+    $('.othercategory li').eq(2).remove();
+    var li = "<li><a href='../view/index.html'>首页</a></li>";
+    $(li).insertBefore($('.othercategory li').eq(0));
+    $('.othercategory li').css({
+        'margin-left': '60px',
+        'margin-top': '3px'
+    });
+    $('.othercategory li').find('a').css('color', '#fff')
+    //左侧导航栏
+    $('.main_left dt').click(function () {
+        $(this).siblings('dd').toggle();
+    })
+    //个人中心通用的样式修改end
+    $('.msg').delegate('.check', 'click', function () {
+        var height = $(this).siblings('.overflow').find('p').css('height');
+        console.log(height)
+        if ($(this).siblings('.overflow').css('height') == '50px') {
+            $(this).siblings('.overflow').css('height', height);
+            $(this).html('收起>>')
+        } else {
+            $(this).siblings('.overflow').css('height', '50px')
+            $(this).html('展开>>')
+        }
+    });
+    $('.content>ul li').delegate('.delete', 'click', function () {
+        var conf = confirm('是否删除此条信息');
+        if (conf) {
+            $(this).parent().remove();
+        }
+
+    })
+})
